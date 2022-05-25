b3becdd90f7d92d09096f37bd2b64cc43cab0773
Sergey Miniaylo <po4tovuj@gmail.com>
2022-05-25 13:54:38 +0300

    Create BookItem.vue

---
+++ src/components/BookItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>{{ book.volumeInfo.title }}</h1>
+        <img :src="book.volumeInfo.imageLinks.thumbnail" :alt="book.volumeInfo.title" />
+        <div class="book-details">
+            <p class="book-details__description">
+                <!-- {{ parsedDescription(String(book.volumeInfo.description)) }} -->
+            </p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType, toRefs, computed } from 'vue';
+import { BookListEntity } from '../types';
+
+export default defineComponent({
+    name: 'BookItem',
+    props: {
+        book: {
+            type: Object as PropType<BookListEntity>,
+            default: () => ({}),
+        },
+    },
+    // setup(props) {
+    //     const { book: MyBook } = toRefs(props);
+    //     console.log('🚀 ~ file: BookItem.vue ~ line 26 ~ setup ~ description', MyBook);
+    //     // const parsedDescription = computed(() =>
+    //     //         return myProps.book.in
+    //     //     );
+    //     return { MyBook };
+    // },
+});
+</script>
